044580d853e0b4598550db89ff224ee352a518b2
majorsaaam <sammy.marques@outlook.com>
2021-05-30 22:41:31 -0300

    Primeiro commit - adicionado RN

---
+++ android/app/src/main/java/com/cattor8ing/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cattor8ing;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "CattoR8ing";
+  }
+}
